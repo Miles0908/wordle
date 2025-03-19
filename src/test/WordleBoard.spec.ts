ee6dcf268ccb3d88a3e891045006550220c580f3
Claudio Ventimiglia <claudioxxmilia@yahoo.it>
2025-03-19 14:18:45 +0100

    only allow player to type letters

--- src/test/WordleBoard.spec.ts
+++ src/test/WordleBoard.spec.ts
@@ -76,7 +76,10 @@ describe("WordleBoard", () => {
       await playerSubmitGuess(wordOfTheDay.toLowerCase())
       expect (wrapper.text()).toContain(VICTORY_MESSAGE)
     })
-    it.todo("player guesses can only contain letters")
+    it("player guesses can only contain letters", async()=>{
+      await playerSubmitGuess("H3!RT")
+      expect(wrapper.find<HTMLInputElement>('input[type="text"]').element.value).toEqual("HRT")
+    })
 
   })
 
