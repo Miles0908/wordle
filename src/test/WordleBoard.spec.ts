369b4b99ef5d04b80369a0bb0a24e80d1559a247
Claudio Ventimiglia <claudioxxmilia@yahoo.it>
2025-03-19 12:23:50 +0100

    arrange phase

--- src/test/WordleBoard.spec.ts
+++ src/test/WordleBoard.spec.ts
@@ -12,21 +12,22 @@ describe("WordleBoard", () => {
     wrapper = mount(WordleBoard, {props:{wordOfTheDay}})
 
   })
-  it("A victory message appear when the user makes a guess that matches the word of the day", async () => {
+  async function playerSubmitGuess(guess: string) {
     const guessInput = wrapper.find("input[type='text']")
-    await guessInput.setValue(wordOfTheDay)
+    await guessInput.setValue(guess)
     await guessInput.trigger("keydown.enter")
+  }
+  it("A victory message appear when the user makes a guess that matches the word of the day", async () => {
 
-
+    await playerSubmitGuess(wordOfTheDay)
     expect(wrapper.text()).toContain(VICTORY_MESSAGE)
+  
   })
   it("A defeat message appear when the user makes a guess that matches the word of the day", async () => {
-    const guessInput = wrapper.find("input[type='text']")
-    await guessInput.setValue("WRONG")
-    await guessInput.trigger("keydown.enter")
 
-
+    await playerSubmitGuess("WRONG")
     expect(wrapper.text()).toContain(DEFEAT_MESSAGE)
+
   })
   it("No end-of-game message appears if the user has not yet made a guess", async () => {
 
